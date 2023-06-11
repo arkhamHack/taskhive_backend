583c6190db517f95cdf240e48c75450baec05273
Avigyan Sinha <72064090+arkhamHack@users.noreply.github.com>
2023-06-11 22:39:53 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ const router=express.Router()
 module.exports=async(app)=>{
     app.use(express.json());
     app.use(cors());
-    dbRoutes(app);
-    userAuth(app);
+    dbRoutes(router);
+    userAuth(router);
+    app.use(router);
     //app.use('/.netlify/functions/api',router);
     //serverless(app);
 
